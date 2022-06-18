930e5c67ace43dd0a1023d62ae310cfe9d5ed5ea
NotBitcoinCEO <96089214+NotBitcoinCEO@users.noreply.github.com>
2022-06-18 10:16:31 -0700

    added polymorphism to snake.py

--- game/casting/snake.py
+++ game/casting/snake.py
@@ -3,7 +3,7 @@ from game.casting.actor import Actor
 from game.shared.point import Point
 
 
-class Snake(Actor):
+class Snake1(Actor):
     """
     A long limbless reptile.
     
@@ -70,3 +70,84 @@ class Snake(Actor):
             segment.set_text(text)
             segment.set_color(color)
             self._segments.append(segment)
+class Snake2(Actor):
+    """
+    A long limbless reptile.
+    
+    The responsibility of Snake is to move itself.
+
+    Attributes:
+        _points (int): The number of points the food is worth.
+    """
+    def __init__(self):
+        super().__init__()
+        self._segments = []
+        self._prepare_body()
+        
+    def get_snake(self):
+        return self._snake
+
+    def get_segments(self):
+        return self._segments
+
+    def move_next(self):
+        # move all segments
+        for segment in self._segments:
+            segment.move_next()
+        # update velocities
+        for i in range(len(self._segments) - 1, 0, -1):
+            trailing = self._segments[i]
+            previous = self._segments[i - 1]
+            velocity = previous.get_velocity()
+            trailing.set_velocity(velocity)
+
+    def get_head(self):
+        return self._segments[0]
+
+    def grow_tail(self, number_of_segments):
+        for i in range(number_of_segments):
+            tail = self._segments[-1]
+            velocity = tail.get_velocity()
+            offset = velocity.reverse()
+            position = tail.get_position().add(offset)
+            
+            segment = Actor()
+            segment.set_position(position)
+            segment.set_velocity(velocity)
+            segment.set_text("#")
+            segment.set_color(constants.GREEN)
+            self._segments.append(segment)
+
+    def turn_head(self, velocity):
+        self._segments[0].set_velocity(velocity)
+    
+    def _prepare_body(self):
+        x = int(constants.MAX_X / 2)
+        y = int(constants.MAX_Y / 2)
+
+        for i in range(constants.SNAKE_LENGTH):
+            position = Point(x - i * constants.CELL_SIZE, y)
+            velocity = Point(1 * constants.CELL_SIZE, 0)
+            text = "8" if i == 0 else "#"
+            color = constants.YELLOW if i == 0 else constants.GREEN
+            
+            segment = Actor()
+            segment.set_position(position)
+            segment.set_velocity(velocity)
+            segment.set_text(text)
+            segment.set_color(color)
+            self._segments.append(segment)
+
+#creating cycles via polymorphism (what do you think?)
+
+obj_snake1 = Snake1()            
+obj_snake2 = Snake2()
+for cycle in (obj_snake1, obj_snake2)
+    cycle.get_snake()
+    cycle.get_segments()
+    cycle.move_next()
+    cycle.get_head()
+    cycle.grow_tail()
+    cycle.turn_head()
+    cycle._prepare_body()
+            
